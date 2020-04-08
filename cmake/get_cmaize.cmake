30064f65fdcd3c7da275a9eef0821890fb2ac446
Blake Mulnix <banjo@pop-os.localdomain>
2020-04-08 17:11:46 -0500

    add if statement for testing and rename get_cpp.cmake to get_cmaize.cpp

--- cmake/get_cmaize.cmake
+++ cmake/get_cmaize.cmake
@@ -1,26 +1,26 @@
 include_guard()
 
 #[[
-# This function encapsulates the process of getting CMakePP using CMake's
+# This function encapsulates the process of getting CMaize using CMake's
 # FetchContent module. We have encapsulated it in a function so we can set
 # the options for it's configure step without affecting the options for the
-# parent project's configure step (namely we do not want to build CMakePP's
+# parent project's configure step (namely we do not want to build CMaize's
 # unit tests).
 #]]
-function(get_cpp)
+function(get_cmaize)
     include(FetchContent)
     set(build_testing_old "${BUILD_TESTING}")  # Store the old value
     set(BUILD_TESTING OFF CACHE BOOL "" FORCE)
     FetchContent_Declare(
-         cpp
-         GIT_REPOSITORY https://github.com/CMakePP/CMakePackagingProject
+         cmaize
+         GIT_REPOSITORY https://github.com/CMakePP/CMaize
     )
-    FetchContent_MakeAvailable(cpp)
+    FetchContent_MakeAvailable(cmaize)
     set(BUILD_TESTING "${build_testing_old}" CACHE BOOL "" FORCE)  # Reset
 endfunction()
 
-# Call the function we just wrote to get CMakePP
-get_cpp()
+# Call the function we just wrote to get CMaize
+get_cmaize()
 
-# Include CMakePP
+# Include CMaize
 include(cpp/cpp)
